e9572df02876f7e80a5085731efda8ffd94742b2
TamaraRomo <78438168+TamaraRomo@users.noreply.github.com>
2024-06-12 19:54:51 -0600

    Initial Commit

---
+++ LabsFLowsApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LabsFLowsApp
+//
+//  Created by Tamara Rodriguez Romo on 12/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LabsFLowsApp/LabsFLowsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabsFLowsAppApp.swift
+//  LabsFLowsApp
+//
+//  Created by Tamara Rodriguez Romo on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LabsFLowsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
